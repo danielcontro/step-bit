81188408e9f2738b86fa0e34005a38e5aee069c9
Davide Albiero <davide2910@live.it>
2023-06-17 15:43:03 +0200

    Pulizia

--- lib/screens/favorites.dart
+++ lib/screens/favorites.dart
@@ -1,39 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:stepbit/database/entities/favorite.dart';
-import 'package:stepbit/models/poi.dart';
-import 'package:stepbit/utils/overpass_api.dart';
-import 'package:stepbit/utils/position.dart';
 import 'package:stepbit/widgets/loading.dart';
 
 import '../repositories/database_repository.dart';
 import '../widgets/favorite_card.dart';
 
 class Favorites extends StatelessWidget {
-  late final double data;
-
-  Favorites({Key? key, required data}) : super(key: key) {
-    this.data = data * 500;
-  }
-
-  Future<List<POI>> buildQuery(double distance) async {
-    final currentPosition = await getCurrentPosition();
-    if (currentPosition != null) {
-      return OverpassApi().query([
-        ('tourism', 'attraction'),
-        ('tourism', 'viewpoint'),
-        ('tourism', 'museum'),
-        ('tourism', 'gallery'),
-        ('building', 'church'),
-        ('building', 'chapel'),
-        ('historic', 'building'),
-        ('amenity', 'restaurant'),
-        ('amenity', 'bar'),
-        ('amenity', 'ice_cream'),
-      ], currentPosition, distance);
-    }
-    return Future.error('Unable to fetch location');
-  }
+  const Favorites({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
