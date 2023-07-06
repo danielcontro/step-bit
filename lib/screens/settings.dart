2d98cd0a27f175e1a3b27e45cf5ca849a0824692
Davide Albiero <davide2910@live.it>
2023-07-06 10:06:51 +0200

    Aggiunto logout

--- lib/screens/settings.dart
+++ lib/screens/settings.dart
@@ -1,5 +1,7 @@
 import 'package:easy_settings/easy_settings.dart';
 import 'package:flutter/material.dart';
+import 'package:stepbit/screens/login.dart';
+import 'package:stepbit/utils/token_manager.dart';
 
 const String dailyStepsGoalKey = "dailyStepsGoal";
 
@@ -14,7 +16,21 @@ List<SettingsCategory> settingsCategories = [
               title: "Set your daily steps goal",
               defaultValue: 8000,
               iconData: Icons.assist_walker),
-        ])
+          ButtonSettingsElement(
+              title: "Quick reset settings",
+              iconData: Icons.restore,
+              onClick: (BuildContext context) => resetSettings()),
+          ButtonSettingsElement(
+              title: "Logout",
+              iconData: Icons.logout,
+              onClick: (BuildContext context) {
+                TokenManager.clearTokens();
+                final nav = Navigator.of(context);
+                nav.pop();
+                Navigator.pushReplacement(context,
+                    MaterialPageRoute(builder: (context) => const Login()));
+              }),
+        ]),
       ])
 ];
 
